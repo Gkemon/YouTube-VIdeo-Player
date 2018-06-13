0a6b1821f6ed467966beaa7aa232c4931146a406
Gk emon <emon.techlogicians@gmail.com>
2018-06-13 16:24:02 +0600

    Initial commit

---
+++ app/src/main/java/com/gkemon/youtubevideoplayer/Config.java
@@ -0,0 +1,12 @@
+package com.gkemon.youtubevideoplayer;
+
+public
+class Config {
+
+    // Google Console APIs developer key
+    // Replace this key with your's
+    public static final String DEVELOPER_KEY = "AIzaSyBRKXbmiX2l1b-0qV0oZ0FwQhxJU0xE6QQ";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
